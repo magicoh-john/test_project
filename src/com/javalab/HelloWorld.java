f725c8bd3f827248d33ac7a9c7599e21ebf2e43d
magicoh-john <61308861+magicoh-john@users.noreply.github.com>
2023-07-29 14:54:48 +0900

    Update HelloWorld.java

--- src/com/javalab/HelloWorld.java
+++ src/com/javalab/HelloWorld.java
@@ -3,7 +3,7 @@ package com.javalab;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		// 다시 한줄로
 		System.out.println("Hello World");
 
 	}
